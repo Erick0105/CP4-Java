7afab7d997ebdc555934bef104774ec4ac1df76b
Erick0105 <erickspot01@gmail.com>
2024-08-16 09:56:46 -0300

    Enum de plataformas criada

---
+++ src/enums/PlataformasEnum.java
@@ -0,0 +1,7 @@
+package enums;
+
+public enum PlataformasEnum {
+    PC,
+    CELULAR,
+    CONSOLE
+}
--- src/models/Jogos.java
+++ src/models/Jogos.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 
 public class Jogos {
 
+    //Atributos
     private String nome;
     private int lancamento;
     private double preco;
@@ -12,50 +13,51 @@ public class Jogos {
     private Boolean sequencia;
     private ArrayList<String> plataforma;
 
+
+    //Getters e Setters
     public String getNome() {
         return nome;
     }
-
     public void setNome(String nome) {
         this.nome = nome;
     }
 
+
     public int getLancamento() {
         return lancamento;
     }
-
     public void setLancamento(int lancamento) {
         this.lancamento = lancamento;
     }
 
+
     public double getPreco() {
         return preco;
     }
-
     public void setPreco(double preco) {
         this.preco = preco;
     }
 
+
     public GeneroJogoEnum getGenero() {
         return genero;
     }
-
     public void setGenero(GeneroJogoEnum genero) {
         this.genero = genero;
     }
 
+
     public Boolean getSequencia() {
         return sequencia;
     }
-
     public void setSequencia(Boolean sequencia) {
         this.sequencia = sequencia;
     }
 
+
     public ArrayList<String> getPlataforma() {
         return plataforma;
     }
-
     public void setPlataforma(ArrayList<String> plataforma) {
         this.plataforma = plataforma;
     }
