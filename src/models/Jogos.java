5f915092836cafc11d59442ee4a96116c3f79ba2
Erick0105 <erickspot01@gmail.com>
2024-08-16 09:33:41 -0300

    Atributos criados

---
+++ src/models/Jogos.java
@@ -0,0 +1,14 @@
+package models;
+
+import enums.GeneroJogoEnum;
+import java.util.ArrayList;
+
+public class Jogos {
+
+    private String nome;
+    private int lancamento;
+    private double preco;
+    private GeneroJogoEnum genero;
+    private Boolean sequencia;
+    private ArrayList<String> plataforma;
+}
