8b2ed44dd4f8a8413de29fbeb064d21e62623629
LuizHNR <luizhneri12@gmail.com>
2024-08-16 09:38:36 -0300

    get and set

--- src/models/Jogos.java
+++ src/models/Jogos.java
@@ -11,4 +11,52 @@ public class Jogos {
     private GeneroJogoEnum genero;
     private Boolean sequencia;
     private ArrayList<String> plataforma;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getLancamento() {
+        return lancamento;
+    }
+
+    public void setLancamento(int lancamento) {
+        this.lancamento = lancamento;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public GeneroJogoEnum getGenero() {
+        return genero;
+    }
+
+    public void setGenero(GeneroJogoEnum genero) {
+        this.genero = genero;
+    }
+
+    public Boolean getSequencia() {
+        return sequencia;
+    }
+
+    public void setSequencia(Boolean sequencia) {
+        this.sequencia = sequencia;
+    }
+
+    public ArrayList<String> getPlataforma() {
+        return plataforma;
+    }
+
+    public void setPlataforma(ArrayList<String> plataforma) {
+        this.plataforma = plataforma;
+    }
 }
